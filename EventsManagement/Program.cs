af51baaa2efa524f299d3bdc0188af768755c352
grzesikb <grzesikb@wp.pl>
2022-10-26 21:53:17 +0200

    test -Bartek

--- EventsManagement/Program.cs
+++ EventsManagement/Program.cs
@@ -11,8 +11,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
 
+}
+// test
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
